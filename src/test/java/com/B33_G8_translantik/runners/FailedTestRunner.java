3b0b54c37ede10040f239ad746bd0f48562987b6
Audrone S. <audra.mvc@gmail.com>
2024-05-07 18:47:29 -0400

    Fixed the package name on all modules

---
+++ src/test/java/com/B33_G8_translantik/runners/FailedTestRunner.java
@@ -0,0 +1,15 @@
+package com.B33_G8_translantik.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "@target/rerun.txt",
+        glue = "com/fleet/step_definitions"
+)
+public class FailedTestRunner {
+}
+
+
---
+++ src/test/java/com/B33_G8_translantik/step_definitions/pinbarStepDefs.java
@@ -0,0 +1,39 @@
+package com.B33_G8_translantik.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class pinbarStepDefs {
+
+    @Given("User is on translantik home page")
+    public void user_is_on_translantik_home_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User clicks the Learn how to use this space link")
+    public void user_clicks_the_learn_how_to_use_this_space_link() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("User sees How to Use Pinbar and Use the pin icon on the right top corner of page to create fast access link in the pinbar.")
+    public void user_sees_how_to_use_pinbar_and_use_the_pin_icon_on_the_right_top_corner_of_page_to_create_fast_access_link_in_the_pinbar() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("User is on translantik pinbar help page")
+    public void user_is_on_translantik_pinbar_help_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("User sees an image on the page")
+    public void user_sees_an_image_on_the_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
