850e008638b19cacafc23425e3de2363016e7900
Audrone S. <audra.mvc@gmail.com>
2024-05-09 16:04:57 -0400

    Fixed the package name on all modules

--- src/test/java/com/B33_G8_translantik/step_definitions/pinbarStepDefs.java
+++ src/test/java/com/B33_G8_translantik/step_definitions/pinbarStepDefs.java
@@ -6,10 +6,10 @@ import io.cucumber.java.en.When;
 
 public class pinbarStepDefs {
 
-    @Given("User is on translantik home page")
-    public void user_is_on_translantik_home_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @Given("the user logged in as {string}")
+    public void the_user_logged_in_as(String string) {
+
+
     }
 
     @When("User clicks the Learn how to use this space link")
@@ -20,12 +20,6 @@ public class pinbarStepDefs {
 
     @Then("User sees How to Use Pinbar and Use the pin icon on the right top corner of page to create fast access link in the pinbar.")
     public void user_sees_how_to_use_pinbar_and_use_the_pin_icon_on_the_right_top_corner_of_page_to_create_fast_access_link_in_the_pinbar() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Given("User is on translantik pinbar help page")
-    public void user_is_on_translantik_pinbar_help_page() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
