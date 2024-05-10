6b31a66b2dd41fa80e570e6fc3d7b82dccc2f71a
Audrone <81379683+audramvc@users.noreply.github.com>
2024-05-10 14:45:42 -0400

    Update CukesRunner.java

--- src/test/java/com/B33_G8_translantik/runners/CukesRunner.java
+++ src/test/java/com/B33_G8_translantik/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
                 "json:target/cucumber.json"
         },
         features = "src/test/resources/features",
-        glue = "com/fleet/step_definitions",
+        glue = "com/B33_G8_translantik/step_definitions",
         dryRun = true,
         tags = "@wip",
         publish = true //generating a report with public link
