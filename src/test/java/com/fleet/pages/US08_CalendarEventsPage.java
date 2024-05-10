e4c841382342d984b3337b3855832fedf356b76e
MaksymDrizd <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>
2024-05-10 10:13:28 -0400

    finished US08

--- src/test/java/com/fleet/pages/US08_CalendarEventsPage.java
+++ src/test/java/com/fleet/pages/US08_CalendarEventsPage.java
@@ -1,12 +1,28 @@
 package com.fleet.pages;
 
+import com.fleet.utilities.BrowserUtils;
 import com.fleet.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class US08_CalendarEventsPage {
+public class US08_CalendarEventsPage extends BasePage{
 
     public US08_CalendarEventsPage(){
         PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(xpath = "//a[@title='Create Calendar event']")
+    public WebElement createCalendarEventButton;
+    @FindBy(xpath = "//input[@data-name='recurrence-repeat']")
+    public WebElement repeatCheckbox;
+    @FindBy(xpath = "(//input[@class='recurrence-subview-control__number'])[1]")
+    public WebElement intervalInputBox;
+    @FindBy(xpath = "//span[@class='validation-failed']")
+    public WebElement errorMessage;
+
+    public void goToCalendarEventsPage(){
+        navigateToModule("Activities","Calendar Events");
     }
 
 }
--- src/test/java/com/fleet/step_definitions/US08_CalendarEventsStepDefs.java
+++ src/test/java/com/fleet/step_definitions/US08_CalendarEventsStepDefs.java
@@ -1,43 +1,56 @@
 package com.fleet.step_definitions;
 
+import com.fleet.pages.US08_CalendarEventsPage;
+import com.fleet.utilities.BrowserUtils;
+import com.fleet.utilities.Driver;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class US08_CalendarEventsStepDefs {
+    US08_CalendarEventsPage calendarEventsPage = new US08_CalendarEventsPage();
 
-    @Then("User hovers over to Activities module")
-    public void user_hovers_over_to_activities_module() {
-
-
+    @Then("User hovers over to Activities module and selects Calendar Events")
+    public void userHoversOverToActivitiesModuleAndSelectsCalendarEvents() {
+        BrowserUtils.waitFor(3);
+        calendarEventsPage.goToCalendarEventsPage();
+        BrowserUtils.waitFor(3);
     }
-    @Then("User selects Calendar Events")
-    public void user_selects_calendar_events() {
 
-
-    }
     @Then("User on Calendar Events page clicks on Create Calendar event")
     public void user_on_calendar_events_page_clicks_on_create_calendar_event() {
-
+        BrowserUtils.waitForClickablility(calendarEventsPage.createCalendarEventButton,60);
+        calendarEventsPage.createCalendarEventButton.click();
 
     }
     @Then("User clicks on Repeat checkbox")
     public void user_clicks_on_repeat_checkbox() {
-
+        calendarEventsPage.repeatCheckbox.click();
 
     }
     @Then("User can see the number {string} by default in the Repeat Every input option")
-    public void user_can_see_the_number_by_default_in_the_repeat_every_input_option(String string) {
-
+    public void user_can_see_the_number_by_default_in_the_repeat_every_input_option(String expectedDefaultValue) {
+        String actualDefaultValue = calendarEventsPage.intervalInputBox.getAttribute("value");
+        Assert.assertEquals(expectedDefaultValue,actualDefaultValue);
 
     }
 
     @Then("User delete number {int} from Repeat Every input option")
     public void user_delete_number_from_repeat_every_input_option(Integer int1) {
-
+        calendarEventsPage.intervalInputBox.clear();
 
     }
-    @Then("User can see the error message “This value should not be blank.”")
-    public void user_can_see_the_error_message_this_value_should_not_be_blank() {
 
 
+    @And("User can see the error message {string}")
+    public void userCanSeeTheErrorMessage(String expectedErrorMessage) {
+        String actualErrorMessage = calendarEventsPage.errorMessage.getText();
+                calendarEventsPage.errorMessage.isDisplayed();
+        Assert.assertEquals(expectedErrorMessage,actualErrorMessage);
     }
 }
