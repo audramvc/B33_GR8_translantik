07dd7c19f26f068304ac9d0d987e6907296eebfe
Audrone <81379683+audramvc@users.noreply.github.com>
2024-05-10 15:57:31 -0400

    Update BasePage.java

--- src/test/java/com/fleet/pages/BasePage.java
+++ src/test/java/com/fleet/pages/BasePage.java
@@ -1,10 +1,10 @@
-package com.B33_G8_translantik.pages;
+package com.B33_GR8_translantik.pages;
 
 
 
 
-import com.B33_G8_translantik.utilities.BrowserUtils;
-import com.B33_G8_translantik.utilities.Driver;
+import com.B33_GR8_translantik.utilities.BrowserUtils;
+import com.B33_GR8_translantik.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
